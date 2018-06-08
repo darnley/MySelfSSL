84e6384e692bd60de4a1c3c36b7654a095b0b8cf
Darnley Costa <darnley.dasilvavieiracosta@thomsonreuters.com>
2018-06-08 16:08:39 -0300

    Configuração de assembly info.

--- Holbor.MySelfSSL/Holbor.MySelfSSL/Properties/AssemblyInfo.cs
+++ Holbor.MySelfSSL/Holbor.MySelfSSL/Properties/AssemblyInfo.cs
@@ -1,23 +1,24 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Holbor.MySelfSSL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MySelfSSL")]
+[assembly: AssemblyDescription("Create a self-signed certificate")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Darnley Costa")]
 [assembly: AssemblyProduct("Holbor.MySelfSSL")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Darnley Costa, 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ba8d8ef5-1ee9-408b-99f9-4d779ed72749")]
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
